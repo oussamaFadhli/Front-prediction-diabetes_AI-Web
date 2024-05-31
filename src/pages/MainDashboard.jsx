b35076e1e31b0998279d7b9fc0f2e0bf227ede08
Oussama Fadhli <www.oussama55aw5@gmail.com>
2024-05-31 11:51:25 +0100

    configure chart with the api

--- src/pages/MainDashboard.jsx
+++ src/pages/MainDashboard.jsx
@@ -1,23 +1,57 @@
 import { DiabetesRisk, MainDashbordCards } from "../components";
+import { useEffect, useState } from "react";
 import { DashboardLayout } from "../layouts";
-const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Juin'];
-const data = [5, 10, 20, 30, 40,50, 60, 70, 80, 90,100];
-const options = {
-  responsive: true, 
-  scales: {
-    y: { 
-      ticks: {
-        beginAtZero: true,
-      },
-    },
-  },
+import axiosInstance from "../helpers/axios";
+
+const formatReleaseTime = (isoDateString) => {
+  const date = new Date(isoDateString);
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+  
+  return `${hours}:${minutes}:${seconds}`;
 };
 const MainDashboard = () => {
+
+  const [labels, setLabels] = useState([]);
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get('/predict-diabetes/');
+        const apiData = response.data;
+        const recent_data = apiData.slice(-10);
+
+        const labels = recent_data.map(item => formatReleaseTime(item.release_date));
+        const data = recent_data.map(item => item.prediction_percentage);
+
+        setLabels(labels);
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
   return (
     <DashboardLayout>
       <>
         <MainDashbordCards />
-        <DiabetesRisk labels={labels} data={data} options={options}/>
+        <DiabetesRisk labels={labels} data={data} options={options} />
       </>
     </DashboardLayout>
   );
